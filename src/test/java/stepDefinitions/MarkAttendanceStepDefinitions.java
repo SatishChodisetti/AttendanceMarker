e48a6a127e5c36d04fcfe45f3048804820bd17ba
Satish Chodisetti <satishnani1792@gmail.com>
2025-06-24 21:57:28 +0530

    god help me this time

--- src/test/java/stepDefinitions/MarkAttendanceStepDefinitions.java
+++ src/test/java/stepDefinitions/MarkAttendanceStepDefinitions.java
@@ -25,17 +25,19 @@ public class MarkAttendanceStepDefinitions{
 	    ChromeOptions options=new ChromeOptions();
 	    options.addArguments("--headless=new","--disable-gpu","--window-size=1920,1080","--disable-blink-features=AutomationControlled","--no-sandbox","--disable-dev-shm-usage");
 	    driver=new ChromeDriver(options);
+
 	    driver.manage().window().maximize();
+		wait= new WebDriverWait(driver, Duration.ofSeconds(20));
 	    driver.get("https://app.hrone.cloud/app");
 	    driver.findElement(By.xpath("//input[@id='hrone-username']")).sendKeys("9599017537");
 	    driver.findElement(By.xpath("//span[text()=' NEXT ']")).click();
 	    
-	    By ele= By.xpath("//input[@id='hrone-password']");
-	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	    WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(ele));
-	    
-	    driver.findElement(By.xpath("//input[@id='hrone-password']")).sendKeys("Cricket@123");
+
+
+	    WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='hrone-password']")));
+		passwordInput.sendKeys("Cricket@123");
 	    driver.findElement(By.xpath("//span[text()=' LOG IN ']")).click();
+		log("Login Successfull");
 	    
 	    
 	}
